85eb95b09b758075e84a2bfa296c08a0210816e0
SHINTA <shinta.0806.develop@hotmail.com>
2025-11-15 00:42:51 +0900

    プロジェクトファイルを追加。

---
+++ TestIFileDialog/Contracts/Services/IActivationService.cs
@@ -0,0 +1,6 @@
+﻿namespace TestIFileDialog.Contracts.Services;
+
+public interface IActivationService
+{
+    Task ActivateAsync(object activationArgs);
+}
---
+++ TestIFileDialog/Models/LocalSettingsOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace TestIFileDialog.Models;
+
+public class LocalSettingsOptions
+{
+    public string? ApplicationDataFolder
+    {
+        get; set;
+    }
+
+    public string? LocalSettingsFile
+    {
+        get; set;
+    }
+}
