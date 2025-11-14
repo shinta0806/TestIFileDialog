8a2f9e31ae640e00ba104f130c8de8a5e4795b30
SHINTA <shinta.0806.develop@hotmail.com>
2025-11-15 01:51:01 +0900

    FileOpenPicker を実装。

--- TestIFileDialog/ViewModels/MainWindows/MainPageViewModel.cs
+++ TestIFileDialog/ViewModels/MainWindows/MainPageViewModel.cs
@@ -9,12 +9,63 @@
 // ----------------------------------------------------------------------------
 
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.UI.Xaml;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using WinRT.Interop;
 
 namespace TestIFileDialog.ViewModels;
 
 public partial class MainPageViewModel : ObservableRecipient
 {
+	// ====================================================================
+	// コンストラクター
+	// ====================================================================
+
+	/// <summary>
+	/// メインコンストラクター
+	/// </summary>
 	public MainPageViewModel()
 	{
 	}
+
+	// ====================================================================
+	// public プロパティー
+	// ====================================================================
+
+	// --------------------------------------------------------------------
+	// View 通信用のプロパティー
+	// --------------------------------------------------------------------
+
+	// --------------------------------------------------------------------
+	// コマンド
+	// --------------------------------------------------------------------
+
+	#region ButtonFileOpenPickerClickedCommand
+	[RelayCommand]
+	private async Task ButtonFileOpenPickerClicked()
+	{
+		try
+		{
+			FileOpenPicker fileOpenPicker = new();
+			InitializeWithWindow.Initialize(fileOpenPicker, App.MainWindow.GetWindowHandle());
+			fileOpenPicker.FileTypeFilter.Add(".jpg");
+			fileOpenPicker.FileTypeFilter.Add(".png");
+			fileOpenPicker.FileTypeFilter.Add("*");
+
+			StorageFile? file = await fileOpenPicker.PickSingleFileAsync();
+			if (file == null)
+			{
+				return;
+			}
+
+			await App.MainWindow.ShowMessageDialogAsync(file.Path, "開く");
+		}
+		catch (Exception ex)
+		{
+			await App.MainWindow.ShowMessageDialogAsync(ex.Message, "エラー");
+		}
+	}
+	#endregion
 }
