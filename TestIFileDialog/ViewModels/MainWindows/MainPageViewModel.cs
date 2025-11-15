6b8c0af7c55b861d21bed6b9a4e7ebea4322ba45
SHINTA <shinta.0806.develop@hotmail.com>
2025-11-15 10:39:46 +0900

    Microsoft FileOpenPicker を実装。

--- TestIFileDialog/ViewModels/MainWindows/MainPageViewModel.cs
+++ TestIFileDialog/ViewModels/MainWindows/MainPageViewModel.cs
@@ -12,7 +12,6 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
 using Windows.Storage;
-using Windows.Storage.Pickers;
 
 using WinRT.Interop;
 
@@ -49,7 +48,7 @@ public partial class MainPageViewModel : ObservableRecipient
 	{
 		try
 		{
-			FileOpenPicker fileOpenPicker = new();
+			Windows.Storage.Pickers.FileOpenPicker fileOpenPicker = new();
 			InitializeWithWindow.Initialize(fileOpenPicker, App.MainWindow.GetWindowHandle());
 			fileOpenPicker.FileTypeFilter.Add("*");
 			fileOpenPicker.FileTypeFilter.Add(".jpg");
@@ -76,7 +75,7 @@ public partial class MainPageViewModel : ObservableRecipient
 	{
 		try
 		{
-			FolderPicker folderPicker = new();
+			Windows.Storage.Pickers.FolderPicker folderPicker = new();
 			InitializeWithWindow.Initialize(folderPicker, App.MainWindow.GetWindowHandle());
 
 			StorageFolder? folder = await folderPicker.PickSingleFolderAsync();
@@ -100,7 +99,7 @@ public partial class MainPageViewModel : ObservableRecipient
 	{
 		try
 		{
-			FileSavePicker fileSavePicker = new();
+			Windows.Storage.Pickers.FileSavePicker fileSavePicker = new();
 			InitializeWithWindow.Initialize(fileSavePicker, App.MainWindow.GetWindowHandle());
 			fileSavePicker.FileTypeChoices.Add("JPEG 画像", [".jpg"]);
 			fileSavePicker.FileTypeChoices.Add("PNG 画像", [".png"]);
@@ -112,6 +111,32 @@ public partial class MainPageViewModel : ObservableRecipient
 			}
 
 			await App.MainWindow.ShowMessageDialogAsync(file.Path, "ファイルを保存");
+		}
+		catch (Exception ex)
+		{
+			await App.MainWindow.ShowMessageDialogAsync(ex.Message, "エラー");
+		}
+	}
+	#endregion
+
+	#region ButtonFileOpenPicker2ClickedCommand
+	[RelayCommand]
+	private async Task ButtonFileOpenPicker2Clicked()
+	{
+		try
+		{
+			Microsoft.Windows.Storage.Pickers.FileOpenPicker fileOpenPicker = new(App.MainWindow.AppWindow.Id);
+			fileOpenPicker.FileTypeFilter.Add("*");
+			fileOpenPicker.FileTypeFilter.Add(".jpg");
+			fileOpenPicker.FileTypeFilter.Add(".png");
+
+			Microsoft.Windows.Storage.Pickers.PickFileResult? file = await fileOpenPicker.PickSingleFileAsync();
+			if (file == null)
+			{
+				return;
+			}
+
+			await App.MainWindow.ShowMessageDialogAsync(file.Path, "ファイルを開く");
 		}
 		catch (Exception ex)
 		{
